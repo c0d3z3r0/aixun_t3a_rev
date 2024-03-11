665858ff2b96225b7cd9d36d3f3082ab6e4b9189
Michael Niewöhner <foss@mniewoehner.de>
2024-03-11 15:53:06 +0100

    patch: asmpatch: allow patching both full flash and update binaries
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>

--- patch/asmpatch.py
+++ patch/asmpatch.py
@@ -13,6 +13,20 @@ patches = filter(lambda l: patt.match(l), patches)
 p.close()
 
 f = open(sys.argv[2], 'r+b')
+if f.read(4) == b'JCID':
+  print("Detected update image")
+  flashbase += 0x10000 # bootloader
+  flashbase -= 0x100   # update header size
+
+else:
+  f.seek(0x3f800)
+  if f.read(4) == b'JCID':
+    print("Detected flash image")
+
+  else:
+    print("Unknown image. Abort.")
+    sys.exit(1)
+
 for p in patches:
   off, patch = p.split('\t')[0:2]
   off = int(off.strip().rstrip(':'), 16) - flashbase
